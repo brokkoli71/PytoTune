57dd48dfc7026ffd533848a6fabe2bf1bb6f2f91
Mioriarty <moritz.seppelt@gmail.com>
2025-12-02 13:23:48 +0100

    make samples referenced

--- include/pytotune/algorithms/PitchShifter.h
+++ include/pytotune/algorithms/PitchShifter.h
@@ -5,33 +5,33 @@
 #include <vector>
 
 namespace p2t {
-    struct Windowing {
-        int windowSize;
-        int stride;
+struct Windowing {
+    int windowSize;
+    int stride;
 
-        Windowing(int windowSize, int stride) : windowSize(windowSize), stride(stride) {
-        }
+    Windowing(int windowSize, int stride) : windowSize(windowSize), stride(stride) {
+    }
 
-        Windowing(int windowSize, float overlapPercentage)
-            : windowSize(windowSize),
-              stride(static_cast<float>(windowSize) * (1 - overlapPercentage)) {
-        }
+    Windowing(int windowSize, float overlapPercentage)
+        : windowSize(windowSize),
+          stride(static_cast<float>(windowSize) * (1 - overlapPercentage)) {
+    }
 
-        int getOsamp() const {
-            return windowSize / stride;
-        }
-    };
+    int getOsamp() const {
+        return windowSize / stride;
+    }
+};
 
-    class PitchShifter {
-        Windowing windowing;
-        float sampleRate;
+class PitchShifter {
+    Windowing windowing;
+    float sampleRate;
 
-    public:
-        PitchShifter(Windowing windowing, float sampleRate) : windowing(windowing), sampleRate(sampleRate) {
-        }
+   public:
+    PitchShifter(Windowing windowing, float sampleRate) : windowing(windowing), sampleRate(sampleRate) {
+    }
 
-        std::vector<float> run(const std::vector<float> samples, float factor) const;
-    };
-} // namespace p2t
+    std::vector<float> run(const std::vector<float> &samples, float factor) const;
+};
+}  // namespace p2t
 
 #endif  // PYTOTUNE_PITCHSHIFTER_H
