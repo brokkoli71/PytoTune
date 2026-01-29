0874b56fa23e292cde2d65517c8d93d5f29898ed
Mioriarty <moritz.seppelt@gmail.com>
2026-01-29 11:59:05 +0100

    fix the test

--- tests/test_api.cpp
+++ tests/test_api.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
 
-#include <string>
 #include <algorithm>
 #include <cmath>
+#include <string>
 
-#include "test_utils.h"
-#include "pytotune/io/midi_file.h"
 #include "pytotune/api.h"
+#include "pytotune/io/midi_file.h"
+#include "test_utils.h"
 
 TEST(PythonBindingsTest, TuneToMidi) {
     std::string midiFile = constants::TEST_DATA_DIR + "/test.mid";
@@ -15,16 +15,15 @@ TEST(PythonBindingsTest, TuneToMidi) {
 
     EXPECT_NO_THROW({
         p2t::tune_to_midi(wavFile, midiFile, outputFile);
-        });
+    });
 }
 
 TEST(PythonBindingsTest, TuneToScale) {
     std::string wavFile = constants::VOICE_F400_SR4100;
-    std::string scale = "C major";
     std::string outputFile = constants::TEST_OUTPUT_DIR + "/test_tune_to_scale.wav";
-    float tuning = 440.0f;
+    p2t::Scale scale = p2t::Scale::fromName("C major");
 
     EXPECT_NO_THROW({
-        p2t::tune_to_scale(wavFile, scale, tuning, outputFile);
-        });
+        p2t::tune_to_scale(wavFile, scale, outputFile);
+    });
 }
