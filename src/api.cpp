205137d303153f4b5ef4f00119f76c5328c6a30d
Mioriarty <moritz.seppelt@gmail.com>
2026-01-29 11:52:37 +0100

    Add Scale to cli

--- src/api.cpp
+++ src/api.cpp
@@ -1,12 +1,12 @@
 #include "pytotune/api.h"
-#include "pytotune/io/wav_file.h"
-#include "pytotune/io/midi_file.h"
+#include "pytotune/algorithms/pitch_correction_pipeline.h"
 #include "pytotune/data-structures/scale.h"
 #include "pytotune/data-structures/windowing.h"
-#include "pytotune/algorithms/pitch_correction_pipeline.h"
+#include "pytotune/io/midi_file.h"
+#include "pytotune/io/wav_file.h"
 
 namespace p2t {
-    void tune_to_midi(const std::string& wav_path, const std::string& midi_path, const std::string& out_path) {
+    void tune_to_midi(const std::string &wav_path, const std::string &midi_path, const std::string &out_path) {
         auto wav = WavFile::load(wav_path);
         auto midi = MidiFile::load(midi_path);
 
@@ -19,9 +19,9 @@ namespace p2t {
         outWav.store(out_path);
     }
 
-    void tune_to_scale(const std::string& wav_path, const std::string& scale_name, float tuning, const std::string& out_path) {
+    void tune_to_scale(const std::string &wav_path, const Scale &scale,
+                       const std::string &out_path) {
         auto wav = WavFile::load(wav_path);
-        auto scale = Scale::fromName(scale_name, tuning);
 
         int windowSize = 4096;
         int stride = 1024;
