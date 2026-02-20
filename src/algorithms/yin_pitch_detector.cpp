404f5dbb86f15f000d882feb2a2ddd7a93e0c399
brokkoli71 <brokkoli71b@gmail.com>
2026-02-20 18:38:21 +0100

    add cmnd

--- src/algorithms/yin_pitch_detector.cpp
+++ src/algorithms/yin_pitch_detector.cpp
@@ -30,21 +30,21 @@ namespace p2t {
                 diff[tau] = sum;
             }
 
-            // // CMND (Cumulative Mean Normalized Difference Function)
-            // std::vector<float> cmnd(tau_max + 1, 0.0f);
-            // cmnd[0] = 1.0f; // Avoid division by zero
-            // float running_sum = 0.0f;
-            // for (int tau = 1; tau <= tau_max; ++tau)
-            // {
-            //     running_sum += diff[tau];
-            //     cmnd[tau] = diff[tau] * tau / running_sum;
-            // }
+            // CMND (Cumulative Mean Normalized Difference Function)
+            std::vector<float> cmnd(tau_max + 1, 0.0f);
+            cmnd[0] = 1.0f; // Avoid division by zero
+            float running_sum = 0.0f;
+            for (int tau = 1; tau <= tau_max; ++tau)
+            {
+                running_sum += diff[tau];
+                cmnd[tau] = diff[tau] * tau / running_sum;
+            }
 
-            // Find the pitch for this window based on the diff function and threshold
+            // Find the pitch for this window based on the cmnd function and threshold
             int best_tau = tau_min;
             for (int tau = tau_min; tau <= tau_max; ++tau) {
-                if (diff[tau] < diff[best_tau]) best_tau = tau; // as fallback
-                if (diff[tau] < threshold) {
+                if (cmnd[tau] < cmnd[best_tau]) best_tau = tau; // as fallback
+                if (cmnd[tau] < threshold) {
                     best_tau = tau;
                     break;
                 }
@@ -53,19 +53,19 @@ namespace p2t {
             // std::cout << static_cast<float>(audio_buffer->sampleRate) / best_tau << std::endl;
 
             // quadratic interpolation to refine the estimate if best_tau is not at the boundaries
-            auto refined_tau = static_cast<float>(best_tau);
-            if (best_tau > 0 && best_tau < tau_max) {
-                const auto left = diff[best_tau - 1];
-                const auto center = diff[best_tau];
-                const auto right = diff[best_tau + 1];
-
-                auto denom = 2 * (left + right - 2 * center);
-                float correction = 0.0f;
-                if (denom != 0.0f)
-                    correction = (left - right) / denom;
-                refined_tau += correction;
-            }
-            float pitch = static_cast<float>(audio_buffer.sampleRate) / refined_tau;
+            // auto refined_tau = static_cast<float>(best_tau);
+            // if (best_tau > 0 && best_tau < tau_max) {
+            //     const auto left = diff[best_tau - 1];
+            //     const auto center = diff[best_tau];
+            //     const auto right = diff[best_tau + 1];
+            //
+            //     auto denom = 2 * (left + right - 2 * center);
+            //     float correction = 0.0f;
+            //     if (denom != 0.0f)
+            //         correction = (left - right) / denom;
+            //     refined_tau += correction;
+            // }
+            float pitch = static_cast<float>(audio_buffer.sampleRate) / best_tau /decimation_factor ;
             pitchValues.push_back(pitch);
         }
         return {
