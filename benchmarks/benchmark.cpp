21209c732c93ed15954fad2e8666b29f06096661
Mioriarty <moritz.seppelt@gmail.com>
2026-03-09 10:16:04 +0100

    exclude benchmark.cpp if not on linux

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -1,6 +1,8 @@
-#include "perfevent/PerfEvent.hpp"
+#if defined(__linux__)
+
 #include <iostream>
 
+#include "perfevent/PerfEvent.hpp"
 #include "pytotune/algorithms/pitch_correction_pipeline.h"
 
 constexpr auto pitchRange = p2t::VoiceRanges::HUMAN;
@@ -10,26 +12,26 @@ constexpr int stride = 1024;
 const std::string wavPath = "../tests/data/benchmarking/e-minor-singing-10x.wav";
 
 #if USE_HWY
-  constexpr std::string_view HWY_TAG = "_hwy=on";
+constexpr std::string_view HWY_TAG = "_hwy=on";
 #else
-  constexpr std::string_view HWY_TAG = "_hwy=off";
+constexpr std::string_view HWY_TAG = "_hwy=off";
 #endif
 
 #if USE_PREDEFINED_TWIDDLES
-  constexpr std::string_view TWIDDLES_TAG = "tw=on";
+constexpr std::string_view TWIDDLES_TAG = "tw=on";
 #else
-  constexpr std::string_view TWIDDLES_TAG = "tw=off";
+constexpr std::string_view TWIDDLES_TAG = "tw=off";
 #endif
 
 #ifdef REIMPLEMENTED_WINDOWING
-  constexpr std::string_view WINDOWING_TAG = "_win=on";
+constexpr std::string_view WINDOWING_TAG = "_win=on";
 #else
-  constexpr std::string_view WINDOWING_TAG = "_win=off";
+constexpr std::string_view WINDOWING_TAG = "_win=off";
 #endif
 
 PerfEvent e;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc > 2 && std::string(argv[2]) == "false") {
         e.printHeader = false;
     }
@@ -46,13 +48,14 @@ int main(int argc, char* argv[]) {
         p2t::PitchCorrectionPipeline pipeline;
 
         for (int decimation : {1, 2, 4, 8}) {
-            const std::string ompTag = (std::getenv("OMP_NUM_THREADS") && std::string(std::getenv("OMP_NUM_THREADS")) == "1")
-                                       ? "_omp=off" : "_omp=on";
+            const std::string ompTag = (std::getenv("OMP_NUM_THREADS") && std::string(std::getenv("OMP_NUM_THREADS")) ==
+                                                                              "1")
+                                           ? "_omp=off"
+                                           : "_omp=on";
             PerfEventBlock b(e, 1000000, "d" + std::to_string(decimation) + std::string(HWY_TAG) + ompTag);
-            if (decimation>1) e.printHeader = false;
+            if (decimation > 1) e.printHeader = false;
             pipeline.detectPitch(wav, windowing, pitchRange, 0.05f, decimation);
         }
-
     } else if (tag == "correction") {
         auto wav = p2t::WavFile::load(wavPath);
         const auto scale = p2t::Scale::fromName("E minor");
@@ -64,7 +67,6 @@ int main(int argc, char* argv[]) {
 
         PerfEventBlock b(e, 1000000, std::string(TWIDDLES_TAG) + std::string(WINDOWING_TAG));
         pipeline.shiftPitch(wav, windowing, factors);
-
     } else {
         std::cerr << "Unknown benchmark: " << tag << ". Please specify 'detection' or 'correction'" << std::endl;
         return 1;
@@ -72,3 +74,4 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
+#endif
